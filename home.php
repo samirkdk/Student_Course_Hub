73eeb3c39d51770e7fef4eb2d59782c466faf3c0
PunamPooh <punamchochangipun@gmail.com>
2025-03-25 10:02:30 +0100

    Created homepage layout using HTML and styled with CSS.

---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courses</title>
+    <link rel="stylesheet" href="style.css">
+   
+</head>
+<body>
+  
+     
+    <nav>
+        
+        <input type="checkbox"  id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars" id="btn"></i>
+            
+        </label>
+    <label class="logo">XYZ University</label>
+    
+
+    <ul>
+       <li><a class="active" href="#">Home</a></li> 
+       <li><a href="#">Programmes</a></li> 
+       <li><a href="#">Open Days</a></li> 
+       <li><a href="#">About</a></li> 
+       <li><a href="#">Search</a></li> 
+       
+    </ul>
+</nav>
+
+<img src="C:/FinalProject/images/bg.jpg" alt="bg-uni">
+
+  <br><br>
+ 
+  
+</body>
+
+</html>
